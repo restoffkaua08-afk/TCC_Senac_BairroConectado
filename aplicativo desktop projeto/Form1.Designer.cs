86b3645a92a8aa532121420b720d0082320822e0
marcyellenmarques04-wq <marcyellenmarques04@gmail.com>
2026-05-28 08:52:09 -0300

    Add files via upload

---
+++ aplicativo desktop projeto/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace novatentativa_projeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            txtEmail = new TextBox();
+            txtSenha = new TextBox();
+            btnEntrar = new Button();
+            panel1 = new Panel();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            txtEmail.AccessibleRole = AccessibleRole.OutlineButton;
+            txtEmail.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtEmail.AutoCompleteMode = AutoCompleteMode.Append;
+            txtEmail.ForeColor = Color.Gray;
+            txtEmail.Location = new Point(70, 229);
+            txtEmail.Multiline = true;
+            txtEmail.Name = "txtEmail";
+            txtEmail.PlaceholderText = "Digite seu email aqui...";
+            txtEmail.Size = new Size(244, 27);
+            txtEmail.TabIndex = 0;
+            txtEmail.KeyPress += txtEmail_KeyPress;
+            // 
+            // txtSenha
+            // 
+            txtSenha.ForeColor = SystemColors.InactiveBorder;
+            txtSenha.Location = new Point(70, 281);
+            txtSenha.Multiline = true;
+            txtSenha.Name = "txtSenha";
+            txtSenha.PlaceholderText = "Digite sua senha aqui...";
+            txtSenha.Size = new Size(244, 25);
+            txtSenha.TabIndex = 1;
+            txtSenha.TextChanged += txtSenha_TextChanged;
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.BackColor = Color.SeaGreen;
+            btnEntrar.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEntrar.ForeColor = SystemColors.ButtonHighlight;
+            btnEntrar.Location = new Point(70, 341);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(130, 40);
+            btnEntrar.TabIndex = 2;
+            btnEntrar.Text = "Entrar";
+            btnEntrar.UseVisualStyleBackColor = false;
+            btnEntrar.Click += btnEntrar_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(btnEntrar);
+            panel1.Controls.Add(txtSenha);
+            panel1.Controls.Add(txtEmail);
+            panel1.Controls.Add(pictureBox1);
+            panel1.ForeColor = SystemColors.ActiveCaption;
+            panel1.Location = new Point(263, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(375, 485);
+            panel1.TabIndex = 5;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ActiveCaption;
+            label2.Font = new Font("Cambria", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(133, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(105, 28);
+            label2.TabIndex = 5;
+            label2.Text = "ENTRAR";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(123, 36);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(125, 103);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(74, 207);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(800, 486);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TextBox txtEmail;
+        private TextBox txtSenha;
+        private Button btnEntrar;
+        private Panel panel1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Button button1;
+    }
+}
---
+++ aplicativo desktop projeto/dashboard.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace novatentativa_projeto
+{
+    partial class dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(dashboard));
+            gridChamados = new DataGridView();
+            btnAprovar = new Button();
+            btnReprovar = new Button();
+            gridChamados4 = new DataGridView();
+            gridChamados3 = new DataGridView();
+            gridChamados2 = new DataGridView();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            btnReprovar2 = new Button();
+            btnAprovar2 = new Button();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            btnAprovar3 = new Button();
+            btnAprovar4 = new Button();
+            btnReprovar3 = new Button();
+            btnReprovar4 = new Button();
+            textBox4 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)gridChamados).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)gridChamados4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)gridChamados3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)gridChamados2).BeginInit();
+            SuspendLayout();
+            // 
+            // gridChamados
+            // 
+            gridChamados.BackgroundColor = SystemColors.InactiveCaption;
+            gridChamados.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gridChamados.Location = new Point(22, 59);
+            gridChamados.Name = "gridChamados";
+            gridChamados.Size = new Size(347, 119);
+            gridChamados.TabIndex = 0;
+            gridChamados.CellContentClick += gridChamados_CellContentClick;
+            // 
+            // btnAprovar
+            // 
+            btnAprovar.BackColor = Color.Green;
+            btnAprovar.BackgroundImage = (Image)resources.GetObject("btnAprovar.BackgroundImage");
+            btnAprovar.BackgroundImageLayout = ImageLayout.Stretch;
+            btnAprovar.Location = new Point(22, 184);
+            btnAprovar.Name = "btnAprovar";
+            btnAprovar.Size = new Size(38, 36);
+            btnAprovar.TabIndex = 1;
+            btnAprovar.UseVisualStyleBackColor = false;
+            btnAprovar.Click += btnAprovar_Click;
+            // 
+            // btnReprovar
+            // 
+            btnReprovar.BackColor = Color.Firebrick;
+            btnReprovar.BackgroundImage = (Image)resources.GetObject("btnReprovar.BackgroundImage");
+            btnReprovar.BackgroundImageLayout = ImageLayout.Stretch;
+            btnReprovar.Location = new Point(66, 184);
+            btnReprovar.Name = "btnReprovar";
+            btnReprovar.Size = new Size(38, 38);
+            btnReprovar.TabIndex = 2;
+            btnReprovar.UseVisualStyleBackColor = false;
+            btnReprovar.Click += btnReprovar_Click;
+            // 
+            // gridChamados4
+            // 
+            gridChamados4.BackgroundColor = SystemColors.InactiveCaption;
+            gridChamados4.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gridChamados4.Location = new Point(407, 238);
+            gridChamados4.Name = "gridChamados4";
+            gridChamados4.Size = new Size(347, 150);
+            gridChamados4.TabIndex = 3;
+            // 
+            // gridChamados3
+            // 
+            gridChamados3.BackgroundColor = SystemColors.InactiveCaption;
+            gridChamados3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gridChamados3.Location = new Point(24, 238);
+            gridChamados3.Name = "gridChamados3";
+            gridChamados3.Size = new Size(347, 150);
+            gridChamados3.TabIndex = 4;
+            // 
+            // gridChamados2
+            // 
+            gridChamados2.BackgroundColor = SystemColors.InactiveCaption;
+            gridChamados2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gridChamados2.Location = new Point(420, 59);
+            gridChamados2.Name = "gridChamados2";
+            gridChamados2.Size = new Size(352, 115);
+            gridChamados2.TabIndex = 5;
+            gridChamados2.CellContentClick += gridChamados2_CellContentClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.CausesValidation = false;
+            label1.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ActiveCaption;
+            label1.Location = new Point(333, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(133, 24);
+            label1.TabIndex = 6;
+            label1.Text = "CHAMADOS";
+            label1.TextAlign = ContentAlignment.BottomLeft;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(110, 187);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "OBS:";
+            textBox1.Size = new Size(259, 38);
+            textBox1.TabIndex = 7;
+            // 
+            // btnReprovar2
+            // 
+            btnReprovar2.BackColor = Color.Firebrick;
+            btnReprovar2.BackgroundImage = (Image)resources.GetObject("btnReprovar2.BackgroundImage");
+            btnReprovar2.BackgroundImageLayout = ImageLayout.Stretch;
+            btnReprovar2.Location = new Point(469, 187);
+            btnReprovar2.Name = "btnReprovar2";
+            btnReprovar2.Size = new Size(38, 35);
+            btnReprovar2.TabIndex = 8;
+            btnReprovar2.UseVisualStyleBackColor = false;
+            btnReprovar2.Click += btnReprovar2_Click;
+            // 
+            // btnAprovar2
+            // 
+            btnAprovar2.BackColor = Color.Green;
+            btnAprovar2.BackgroundImage = (Image)resources.GetObject("btnAprovar2.BackgroundImage");
+            btnAprovar2.BackgroundImageLayout = ImageLayout.Stretch;
+            btnAprovar2.Location = new Point(425, 185);
+            btnAprovar2.Name = "btnAprovar2";
+            btnAprovar2.Size = new Size(38, 36);
+            btnAprovar2.TabIndex = 9;
+            btnAprovar2.UseVisualStyleBackColor = false;
+            btnAprovar2.Click += btnAprovar2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(513, 184);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "OBS:";
+            textBox2.Size = new Size(259, 38);
+            textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(110, 394);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "OBS:";
+            textBox3.Size = new Size(259, 38);
+            textBox3.TabIndex = 11;
+            // 
+            // btnAprovar3
+            // 
+            btnAprovar3.BackColor = Color.Green;
+            btnAprovar3.BackgroundImage = (Image)resources.GetObject("btnAprovar3.BackgroundImage");
+            btnAprovar3.BackgroundImageLayout = ImageLayout.Stretch;
+            btnAprovar3.Location = new Point(24, 394);
+            btnAprovar3.Name = "btnAprovar3";
+            btnAprovar3.Size = new Size(38, 36);
+            btnAprovar3.TabIndex = 12;
+            btnAprovar3.UseVisualStyleBackColor = false;
+            btnAprovar3.Click += btnAprovar3_Click;
+            // 
+            // btnAprovar4
+            // 
+            btnAprovar4.BackColor = Color.Green;
+            btnAprovar4.BackgroundImage = (Image)resources.GetObject("btnAprovar4.BackgroundImage");
+            btnAprovar4.BackgroundImageLayout = ImageLayout.Stretch;
+            btnAprovar4.Location = new Point(420, 393);
+            btnAprovar4.Name = "btnAprovar4";
+            btnAprovar4.Size = new Size(38, 36);
+            btnAprovar4.TabIndex = 13;
+            btnAprovar4.UseVisualStyleBackColor = false;
+            btnAprovar4.Click += btnAprovar4_Click;
+            // 
+            // btnReprovar3
+            // 
+            btnReprovar3.BackColor = Color.Firebrick;
+            btnReprovar3.BackgroundImage = (Image)resources.GetObject("btnReprovar3.BackgroundImage");
+            btnReprovar3.BackgroundImageLayout = ImageLayout.Stretch;
+            btnReprovar3.Location = new Point(66, 394);
+            btnReprovar3.Name = "btnReprovar3";
+            btnReprovar3.Size = new Size(38, 35);
+            btnReprovar3.TabIndex = 14;
+            btnReprovar3.UseVisualStyleBackColor = false;
+            btnReprovar3.Click += btnReprovar3_Click;
+            // 
+            // btnReprovar4
+            // 
+            btnReprovar4.BackColor = Color.Firebrick;
+            btnReprovar4.BackgroundImage = (Image)resources.GetObject("btnReprovar4.BackgroundImage");
+            btnReprovar4.BackgroundImageLayout = ImageLayout.Stretch;
+            btnReprovar4.Location = new Point(469, 394);
+            btnReprovar4.Name = "btnReprovar4";
+            btnReprovar4.Size = new Size(38, 35);
+            btnReprovar4.TabIndex = 15;
+            btnReprovar4.UseVisualStyleBackColor = false;
+            btnReprovar4.Click += btnReprovar4_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(513, 394);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "OBS:";
+            textBox4.Size = new Size(239, 38);
+            textBox4.TabIndex = 16;
+            // 
+            // dashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 468);
+            Controls.Add(textBox4);
+            Controls.Add(btnReprovar4);
+            Controls.Add(btnReprovar3);
+            Controls.Add(btnAprovar4);
+            Controls.Add(btnAprovar3);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(btnAprovar2);
+            Controls.Add(btnReprovar2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(gridChamados2);
+            Controls.Add(gridChamados3);
+            Controls.Add(gridChamados4);
+            Controls.Add(btnReprovar);
+            Controls.Add(btnAprovar);
+            Controls.Add(gridChamados);
+            Name = "dashboard";
+            Text = "dashboard";
+            Load += dashboard_Load;
+            ((System.ComponentModel.ISupportInitialize)gridChamados).EndInit();
+            ((System.ComponentModel.ISupportInitialize)gridChamados4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)gridChamados3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)gridChamados2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView gridChamados;
+        private Button btnAprovar;
+        private Button btnReprovar;
+        private DataGridView gridChamados4;
+        private DataGridView gridChamados3;
+        private DataGridView gridChamados2;
+        private Label label1;
+        private TextBox textBox1;
+        private Button btnReprovar2;
+        private Button btnAprovar2;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button btnAprovar3;
+        private Button btnAprovar4;
+        private Button btnReprovar3;
+        private Button btnReprovar4;
+        private TextBox textBox4;
+    }
+}
